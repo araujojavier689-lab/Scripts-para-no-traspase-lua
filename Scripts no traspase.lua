1010406350cc9e84e71cca1e023d0ce23d1130e2
araujojavier689-lab <araujojavier689@gmail.com>
2026-03-30 11:43:10 -0500

    Create initial Git setup script for Lua project
    Initialize Git repository and set up remote origin.

---
+++ Scripts no traspase.lua
@@ -0,0 +1,9 @@
+echo "# Scripts-para-no-traspase-lua" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/araujojavier689-lab/Scripts-para-no-traspase-lua.git
+git push -u origin maingit remote add origin https://github.com/araujojavier689-lab/Scripts-para-no-traspase-lua.git
+git branch -M main
+git push -u origin main
